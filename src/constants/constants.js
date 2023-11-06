a459ca9314fd1ec482b1f8ff583c8794a63a2133
shivam-batham <rsb7214@gmail.com>
2023-11-06 19:00:31 +0530

    project add

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,49 +1,24 @@
 export const projects = [
-  {
-    title: "EmSender",
-    description:
-      "I created a full-stack web application using React, Redux, Express, and MongoDB as the core technologies. The project includes features such as Google OAuth authentication, email notifications, survey collection, and credit card payment processing. This demonstrates my proficiency in building and deploying web applications, handling user authentication, email communication, and online payments.",
-    image: ["/images/emsenderMain.png","/images/emsenderAbout.png","/images/emsenderSurvey.png"],
-    tags: [
-      "Javascript",
-      "Express.js",
-      "Reducx",
-      "React.js",
-      "Node.js",
-      "Tailwind CSS",
-      "MongoDB",
-    ],
-    source: "https://github.com/shadatr/node-with-react",
-    id: 0,
-  },
-  {
-    title: "MediaMingle",
-    description:
-      "I developed a dynamic and real-time social media platform using Next.js and Supabase Realtime. This project showcases my ability to create modern web applications with real-time updates and data synchronization. It includes features like user authentication,real-time chat,profiles, post creation, commenting, liking, sitting and instant notifications, demonstrating my skills in building engaging and interactive web applications using cutting-edge technologies. Demo user: username:user12345 password:12345678",
-    image:[ "/images/mediaMingleMain.png"],
-    tags: ["React.js", "NextJs 13", "Tailwind CSS", "PostgreSQL", "TypeScript"],
-    source: "https://github.com/shadatr/media-mingle",
-    visit: "https://media-mingle-eight.vercel.app/",
-    id: 1,
-  },
+ 
   {
     title: "TechMarket",
     description:
       "Developed and enhanced an e-commerce platform catering to tech products such as phones, laptops, and electronics. Implemented features like product catalog, user accounts, secure payment processing, and order tracking. Continuously monitored site performance, resolved technical issues promptly, and conducted regular updates to enhance functionality and security. Contributed to a successful digital storefront that effectively showcased tech products and drove sales.",
     image: ["/images/e-commercial-shop.png"],
-    tags: ["React.js", "NextJs 13", "Tailwind CSS", "PostgreSQL", "TypeScript"],
-    source: "https://github.com/shadatr/e-commercial-store",
-    visit: "https://selling-website.vercel.app/",
+    tags: ["React.js", "NextJs 13", "Tailwind CSS", "PostgreSQL", "JavaScript"],
+    source: "https://github.com/Shivam-Batham",
+    visit: "https://github.com/Shivam-Batham",
     id: 3,
   },
+ 
   {
-    title: "Resturant Website",
+    title: "Tic Tac Toe Game",
     description:
-      "The project is a fully responsive modern restaurant application designed to provide to show design abilities. It includes features such as a responsive layout, a video demonstration, menu display, online ordering, table reservation, interactive elements. The application showcases your skills in web development, highlighting your ability to create visually appealing and functional applications for the restaurant industry.",
-    image: ["/images/restaurant.png"],
-    tags: ["React", "WebRTC"],
-    source: "https://github.com/shadatr/restaurant-website",
-    visit: "https://restaurant-website-plum.vercel.app/",
+    "This is a Tic Tac Toe game website, Tic Tac Toe is a game in which two players take turns in drawing either an `O` or `X` in one square of a grid consisting of 9 squares.The winner is the 1st player to get 3 of the same symbols in a row.",
+    image: ["/images/tic.png"],
+    tags: ["HTML", "CSS","JavaScript"],
+    source: "https://github.com/Shivam-Batham/Mini_Project-MPGI",
+    visit: "https://shivam-batham.github.io/Mini_Project-MPGI/",
     id: 4,
   },
 ];
@@ -55,3 +30,15 @@ export const TimeLineData = [
   { year: 2020, text: 'Shared my projects with the world', },
   { year: 2021, text: 'Started my own platform', },
 ];
+
+// working on it
+// { 
+//   title: "Resturant Website",
+//   description:
+//     "The project is a fully responsive modern restaurant application designed to provide to show design abilities. It includes features such as a responsive layout, a video demonstration, menu display, online ordering, table reservation, interactive elements. The application showcases your skills in web development, highlighting your ability to create visually appealing and functional applications for the restaurant industry.",
+//   image: ["/images/restaurant.png"],
+//   tags: ["React", "WebRTC","JavaScript"],
+//   source: "https://github.com/Shivam-Batham",
+//   visit: "https://github.com/Shivam-Batham",
+//   id: 4,
+// },
